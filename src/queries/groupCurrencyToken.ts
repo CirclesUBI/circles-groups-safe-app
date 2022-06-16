24a5cbbb79c9a48166b9aa9bb0898c395546a611
Marcelo Rubini <rubini.marcelo@gmail.com>
2022-06-16 18:15:51 -0300

    defines first aproach query

---
+++ src/queries/groupCurrencyToken.ts
@@ -0,0 +1,46 @@
+import gql from 'graphql-tag'
+
+export const GROUP_CURRENCY_TOKEN_QUERY = gql`
+  query groupCurrencyTokens {
+    groupCurrencyTokens {
+      id
+      name
+      symbol
+      creator
+      hub
+      owner
+      treasury
+      mintFeePerThousand
+      suspended
+      onlyOwnerCanMint
+      onlyTrustedCanMint
+    }
+  }
+`
+
+// export const EXAMPLE_QUERY = gql`
+//   query example {
+//     example {
+//       id
+//       proxyAddress
+//     }
+//   }
+// `
+
+// export const EXAMPLE_QUERY_BY_ID = gql`
+//   query exampleById($id: ID!) {
+//     example(id: $id) {
+//       id
+//       proxyAddress
+//     }
+//   }
+// `
+// // where schema will be got from yarn schema script
+// export const EXAMPLE_QUERY_WHERE = gql`
+//   query exampleWhere($where: __Schema) {
+//     example(where: $where) {
+//       id
+//       proxyAddress
+//     }
+//   }
+// `
