72ec4f4a21b28bdc32b8706b036a0e642f190f2c
Marcelo Rubini <rubini.marcelo@gmail.com>
2022-06-21 11:47:03 -0300

    updates GroupCurrencyToken query

--- src/queries/groupCurrencyToken.ts
+++ src/queries/groupCurrencyToken.ts
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag'
 
 export const GROUP_CURRENCY_TOKEN_QUERY = gql`
-  query groupCurrencyTokens {
-    groupCurrencyTokens {
+  query GroupCurrencyTokens($where: GroupCurrencyToken_filter) {
+    groupCurrencyTokens(where: $where) {
       id
       name
       symbol
@@ -11,36 +11,10 @@ export const GROUP_CURRENCY_TOKEN_QUERY = gql`
       owner
       treasury
       mintFeePerThousand
+      minted
       suspended
       onlyOwnerCanMint
       onlyTrustedCanMint
     }
   }
 `
-
-// export const EXAMPLE_QUERY = gql`
-//   query example {
-//     example {
-//       id
-//       proxyAddress
-//     }
-//   }
-// `
-
-// export const EXAMPLE_QUERY_BY_ID = gql`
-//   query exampleById($id: ID!) {
-//     example(id: $id) {
-//       id
-//       proxyAddress
-//     }
-//   }
-// `
-// // where schema will be got from yarn schema script
-// export const EXAMPLE_QUERY_WHERE = gql`
-//   query exampleWhere($where: __Schema) {
-//     example(where: $where) {
-//       id
-//       proxyAddress
-//     }
-//   }
-// `
