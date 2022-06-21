97fa1dd8b60172bf25e654a13b0b9f7c39f3c217
Marcelo Rubini <rubini.marcelo@gmail.com>
2022-06-21 11:58:11 -0300

    updates hook for fetching GroupCurrencyTokens from the subgraph

--- src/hooks/subgraph/useGroupCurrencyToken.tsx
+++ src/hooks/subgraph/useGroupCurrencyToken.tsx
@@ -1,20 +1,16 @@
 import useSWR from 'swr'
 
-// import { GROUP_CURRENCY_TOKEN_QUERY } from '@/src/queries/groupCurrencyToken'
-// import { graphqlFetcher } from '@/src/utils/graphqlFetcher'
-// import { groupCurrencyTokens } from '@/types/subgraph/__generated__/groupCurrencyTokens'
+import { GROUP_CURRENCY_TOKEN_QUERY } from '@/src/queries/groupCurrencyToken'
+import { graphqlFetcher } from '@/src/utils/graphqlFetcher'
+import {
+  GroupCurrencyTokens,
+  GroupCurrencyTokensVariables,
+} from '@/types/subgraph/__generated__/GroupCurrencyTokens'
 
-// export const fetchGroupCurrencyTokens = () =>
-//   graphqlFetcher<groupCurrencyTokens, GroupCurrencyTokensVariables>(
-//     GROUP_CURRENCY_TOKEN_QUERY,
-//     {},
-//   ).then((groupCurrencyTokens) => {
-//     console.log({ groupCurrencyTokens })
-//     return groupCurrencyTokens
-//   })
+export const fetchGroupCurrencyTokens = () =>
+  graphqlFetcher<GroupCurrencyTokens, GroupCurrencyTokensVariables>(GROUP_CURRENCY_TOKEN_QUERY)
 
-// export const useGroupCurrencyTokens = () => {
-//   const { data, error, mutate } = useSWR(['groupCurrencyTokens'], () => fetchGroupCurrencyTokens())
-
-//   return { groupCurrencyTokens: data, error, refetch: mutate, loading: !error && !data }
-// }
+export const useGroupCurrencyTokens = () => {
+  const { data, error, mutate } = useSWR(['groupCurrencyTokens'], () => fetchGroupCurrencyTokens())
+  return { groupCurrencyTokens: data, error, refetch: mutate, loading: !error && !data }
+}
