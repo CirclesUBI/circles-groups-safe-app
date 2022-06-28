554fa61c019928b65e41aa9e36dc0d71e3c6defb
Shisus <jesus.laime@camba.coop>
2022-06-28 18:57:06 -0300

    feat: use safeAddress instead of N/A when we dont get information from the circles garden api

--- src/hooks/useUserSafe.ts
+++ src/hooks/useUserSafe.ts
@@ -1,22 +1,19 @@
 import useSWR from 'swr'
 
-import { getUsers } from '../utils/circlesGardenAPI'
+import { CirclesGardenUser, getUsers } from '../utils/circlesGardenAPI'
 
 export const useUserSafe = (safeAddress: string) => {
   const { data, error, mutate } = useSWR(['useUserSafe', safeAddress], () =>
     getUsers([safeAddress]),
   )
   const result = { error, refetch: mutate }
-  if (data?.length === 0) {
-    return {
-      ...result,
-      user: {
-        id: 0,
-        username: 'N/A',
-        safeAddress,
-      },
-    }
+  let user: CirclesGardenUser = {
+    id: 0,
+    username: safeAddress,
+    safeAddress,
   }
-  const user = data && data[0]
+  if (data && data.length > 0) {
+    user = data[0]
+  }
   return { ...result, user }
 }
