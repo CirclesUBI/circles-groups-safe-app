ce91cebe1415c8f7c823d3d7e48fe84d20241d30
Shisus <jesus.laime@camba.coop>
2022-06-29 13:05:30 -0300

    feat: reduce duplicated code, adds explorer link for the notification

--- src/hooks/useSafeTransaction.tsx
+++ src/hooks/useSafeTransaction.tsx
@@ -4,73 +4,57 @@ import { BaseTransaction, SendTransactionsResponse } from '@gnosis.pm/safe-apps-
 import SafeAppsSDK from '@gnosis.pm/safe-apps-sdk/dist/src/sdk'
 import { toast } from 'react-hot-toast'
 
+import { Chains, chainsConfig } from '../constants/chains'
 import { notify } from '@/src/components/toast/Toast'
 import { FAILED_TYPE, SUCCESS_TYPE, WAITING_TYPE } from '@/src/components/toast/types'
-import { TransactionError } from '@/src/utils/TransactionError'
+import { createTransactionError } from '@/src/utils/TransactionError'
 
-export default function useSafeTransaction() {
+const getGnosisExplorerUrl = (hash: string) => {
+  const url = chainsConfig[Chains.gnosis]?.blockExplorerUrls[0]
+  const type = hash.length > 42 ? 'tx' : 'address'
+  return `${url}${type}/${hash}`
+}
+
+export default function useSafeTransaction(sdk: SafeAppsSDK) {
   const execute = useCallback(
-    async (
-      sdk: SafeAppsSDK,
-      txs: BaseTransaction[],
-      onSuccess?: () => void,
-      onError?: () => void,
-    ) => {
+    async (txs: BaseTransaction[], onSuccess?: () => void, onError?: () => void) => {
       if (!sdk) {
-        // TODO replace console with some notification or toast
         console.error('Transaction failed, there is no sdk')
         return null
       }
 
       let txResponse: SendTransactionsResponse
+      let txHash: string
+      let txExplorerUrl
       try {
         console.info('Please sign the transaction.')
         notify({ type: WAITING_TYPE, explorerUrl: 'waiting for approval' })
         txResponse = await sdk.txs.send({
           txs,
         })
-      } catch (e: any) {
-        toast.dismiss()
-
-        const error = new TransactionError(
-          e.data?.message || e.message || 'Unable to decode revert reason',
-          e.data?.code || e.code,
-          e.data,
-        )
-        if (error.code === 4001) {
-          notify({ type: FAILED_TYPE, message: 'User denied signature' })
-          if (onError) onError()
-          return null
-        }
-        console.error('Transaction error', error.message)
-
-        notify({ type: FAILED_TYPE, message: error.message })
-        if (onError) onError()
-        return null
-      }
-
-      try {
-        notify({ type: SUCCESS_TYPE, explorerUrl: txResponse.safeTxHash })
+        txHash = txResponse.safeTxHash
+        txExplorerUrl = getGnosisExplorerUrl(txHash)
+        notify({ type: SUCCESS_TYPE, explorerUrl: txExplorerUrl })
         toast.dismiss()
         if (onSuccess) onSuccess()
         return txResponse.safeTxHash
       } catch (e: any) {
         toast.dismiss()
 
-        const error = new TransactionError(
-          e.data?.message || e.message || 'Unable to decode revert reason',
-          e.data?.code || e.code,
-          e.data,
-        )
-
-        notify({ type: FAILED_TYPE, explorerUrl: txResponse.safeTxHash })
-
+        const error = createTransactionError(e)
+        if (error.code === 4001) {
+          notify({ type: FAILED_TYPE, message: 'User denied signature' })
+          if (onError) onError()
+          return null
+        }
         console.error('Transaction error', error.message)
+        // notify({ type: FAILED_TYPE, explorerUrl: txExplorerUrl })
+        notify({ type: FAILED_TYPE, message: error.message })
         if (onError) onError()
         return null
       }
     },
-    [],
+    [sdk],
   )
 
   return useMemo(() => {
