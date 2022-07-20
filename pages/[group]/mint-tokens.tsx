d0c3a693c3512b86a8a2b5e8325ca465de5d257d
Tomas Löffler <tomas@bootnode.dev>
2022-07-20 13:43:13 -0300

    Mint tokens link url modification (#125)

--- pages/[group]/mint-tokens.tsx
+++ pages/[group]/mint-tokens.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 import styled from 'styled-components'
