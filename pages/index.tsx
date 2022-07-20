66a8f7005368e5f9576ffcbc967c34aebfc76dae
Tomas Löffler <tomas@bootnode.dev>
2022-07-20 15:49:27 -0300

    Enhancement/#110 mygroups allgroups tabs (#118)
    * Tabs with context / provider
    
    * Tab persistent
    
    * Page transition problem
    
    * tabs Pr fixes
    
    * Remove console log
    
    * Simplify code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
 import type { NextPage } from 'next'
 import styled from 'styled-components'
 
-import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk'
-import { AnimatePresence, motion } from 'framer-motion'
-
-import { Title } from '@/src/components/assets/Title'
-import { GroupList } from '@/src/components/lists/GroupList'
-import { useGroupCurrencyTokens } from '@/src/hooks/subgraph/useGroupCurrencyToken'
-import { useGroupsByMember } from '@/src/hooks/subgraph/useGroupsByMember'
+import { TabContent } from '@/src/components/tabs/TabContent'
+import { TabHeader } from '@/src/components/tabs/TabHeader'
+import GroupsTabs from '@/src/hooks/useGroupsTabs'
 
 const Nav = styled.nav`
   align-items: center;
@@ -21,64 +17,21 @@ const Nav = styled.nav`
     font-weight: 400;
   }
 `
-const Tab = styled.button`
-  background-color: transparent;
-  border: none;
-  color: ${({ theme }) => theme.colors.primary};
-  cursor: pointer;
-  font-size: 18px;
-  font-weight: 400;
-  padding: 0;
-  span {
-    &:not(.active) {
-      opacity: 0.5;
-    }
-  }
-`
-interface tab {
-  text: string
-}
 
-interface Props {
-  tabs: tab[]
-  onChange: (text: string) => void
-  selectedTab: string
-}
+const Home: NextPage = () => {
+  const { Tabs } = GroupsTabs()
 
-const Home: NextPage<Props> = ({ onChange, selectedTab, tabs }) => {
-  const { safe } = useSafeAppsSDK()
-  const { groups } = useGroupCurrencyTokens()
-  const { groupsByMember } = useGroupsByMember(safe.safeAddress)
   return (
     <>
-      <div className="groupsMenu">
-        <Nav>
-          {tabs.map(({ text }, index) => (
-            <Tab key={`tab_${index}`} onClick={() => onChange(text)}>
-              <span className={selectedTab == text ? 'active' : 'inactive'}>{text}</span>
-            </Tab>
-          ))}
-        </Nav>
-      </div>
-      <Title text={selectedTab == 'My Groups' ? 'Groups where i belong' : 'All existing Groups'} />
+      <Nav>
+        {Tabs.map(({ title }, index) => (
+          <TabHeader key={index} title={title} />
+        ))}
+      </Nav>
 
-      <section>
-        <AnimatePresence exitBeforeEnter>
-          <motion.div
-            animate={{ x: 0, opacity: 1 }}
-            exit={{ x: 10, opacity: 0 }}
-            initial={{ x: -20, opacity: 0 }}
-            key={selectedTab ? selectedTab : 'empty'}
-            transition={{ duration: 0.2 }}
-          >
-            {selectedTab == 'My Groups' ? (
-              <GroupList groups={groupsByMember} />
-            ) : (
-              <GroupList groups={groups} />
-            )}
-          </motion.div>
-        </AnimatePresence>
-      </section>
+      {Tabs.map(({ content, header, title }, index) => (
+        <TabContent content={content} header={header} key={index} whenActive={title} />
+      ))}
     </>
   )
 }
---
+++ src/components/tabs/TabContent.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode } from 'react'
+
+import { AnimatePresence, motion } from 'framer-motion'
+
+import { Title } from '@/src/components/assets/Title'
+import { useTab } from '@/src/providers/tabProvider'
+
+interface Props {
+  content: ReactNode
+  header: string
+  whenActive: string
+}
+
+export const TabContent: React.FC<Props> = ({ content, header, whenActive }) => {
+  const { activeTab } = useTab()
+  return (
+    <>
+      {whenActive == activeTab && (
+        <>
+          <AnimatePresence exitBeforeEnter>
+            <motion.div
+              animate={{ x: 0, opacity: 1 }}
+              exit={{ x: 10, opacity: 0 }}
+              initial={{ x: -10, opacity: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Title text={header} />
+            </motion.div>
+          </AnimatePresence>
+          <AnimatePresence exitBeforeEnter>
+            <motion.div
+              animate={{ x: 0, opacity: 1 }}
+              exit={{ x: 10, opacity: 0 }}
+              initial={{ x: -20, opacity: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              {content}
+            </motion.div>
+          </AnimatePresence>
+        </>
+      )}
+    </>
+  )
+}
