2e580377411ffb34376de8048b72eac329835887
Shisus <jesus.laime@camba.coop>
2022-07-26 15:55:59 -0300

    fixes convert mint amount from circles to tc before sending to mint, max mint amount is not converted to tc

--- src/hooks/useGroupMintToken.ts
+++ src/hooks/useGroupMintToken.ts
@@ -5,7 +5,7 @@ import SafeAppsSDK from '@gnosis.pm/safe-apps-sdk/dist/src/sdk'
 import useSWR from 'swr'
 
 import { useWeb3Connected } from '../providers/web3ConnectionProvider'
-import { circlesToTC } from '../utils/circleConversor'
+import { tcToCircles } from '../utils/circleConversor'
 import encodeGCTTransaction from '../utils/contracts/encodeGCTTransaction'
 import encodeHubTransaction from '../utils/contracts/encodeHubTransaction'
 import {
@@ -13,14 +13,16 @@ import {
   transformPathToMintParams,
   transformPathToTransferThroughParams,
 } from '../utils/pathfinderAPI'
-import { toFreckles } from '../web3/bigNumber'
+import { formatToken, toFreckles } from '../web3/bigNumber'
 import { useGroupCurrencyTokensById } from './subgraph/useGroupCurrencyToken'
 import useSafeTransaction from './useSafeTransaction'
 
-const fetchGroupMintTokenData = async (from: string, to: string) => {
+const fetchGroupMintTokenData = async (from: string, to: string, amount?: string) => {
+  if (!from || !to) return { path: [], mintMaxAmount: '0' }
+
   const checksumFromAddress = getAddress(from)
   const checksumToAddress = getAddress(to)
-  const data = await getPath(checksumFromAddress, checksumToAddress)
+  const data = await getPath(checksumFromAddress, checksumToAddress, amount)
   const path = data?.transfers ?? []
   const mintMaxAmount = data?.flow ?? '0'
   return { path, mintMaxAmount }
@@ -55,11 +57,26 @@ export const useGroupMintToken = (userAddress: string, groupAddress: string, sdk
         // @TODO we can use a default provider instead of relaying on the web3 provider
         const provider = web3Provider.getSigner()
 
-        const path = mintTokenData?.path
-        if (!path) {
+        const formattedMintAmount = toFreckles(mintAmount)
+
+        console.log('................ fetching path from params')
+        console.log({ userAddress })
+        console.log({ groupAddress })
+        console.log({ formattedMintAmount })
+        const { mintMaxAmount, path } = await fetchGroupMintTokenData(
+          userAddress,
+          groupAddress,
+          formattedMintAmount,
+        )
+        console.log('................ result path from query')
+        console.log({ path })
+        console.log({ mintMaxAmount })
+        if (path.length === 0) {
           throw new Error('Path does not exists yet')
         }
+        // @TODO we should verify that new max amount is greater or equals than expected mint amount
         const { dests, srcs, tokenOwners, wads } = transformPathToTransferThroughParams(path)
+
         const transferThroughTx = await encodeHubTransaction(provider, 'transferThrough', [
           tokenOwners,
           srcs,
@@ -73,8 +90,11 @@ export const useGroupMintToken = (userAddress: string, groupAddress: string, sdk
          */
         const users = [userAddress]
         const collaterals = await transformPathToMintParams(users, provider)
-        const formattedAmount = toFreckles(mintAmount)
-        const amounts = [formattedAmount]
+        // @TODO shall we convert mintAmount to tc?
+        // const tcMintAmount = formattedMintAmount
+        const tcMintAmount = String(tcToCircles(formattedMintAmount))
+        console.log({ tcMintAmount })
+        const amounts = [formattedMintAmount]
         if (collaterals.length === 0) {
           throw new Error('Collaterals must have some elements')
         }
@@ -90,11 +110,11 @@ export const useGroupMintToken = (userAddress: string, groupAddress: string, sdk
         setLoading(false)
       }
     },
-    [group, isAppConnected, web3Provider, mintTokenData, execute, groupAddress, userAddress],
+    [group, isAppConnected, web3Provider, execute, groupAddress, userAddress],
   )
   return {
-    path: mintTokenData?.path,
-    mintMaxAmount: circlesToTC(mintTokenData?.mintMaxAmount),
+    // @TODO we don't convert anymore from circles to tc, investigate if this is correct
+    mintMaxAmount: formatToken(mintTokenData?.mintMaxAmount) ?? '0',
     error,
     refetch: mutate,
     mintToken,
