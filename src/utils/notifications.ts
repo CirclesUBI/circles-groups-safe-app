cce729a1090b579c400eef451ffd120049e06a57
Shisus <jesus.laime@camba.coop>
2022-08-04 17:26:42 -0300

    adds format of group events into a text message

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,11 +1,6 @@
-import { ZERO_ADDRESS } from '../constants/misc'
-import {
-  UserNotification,
-  UserNotificationHubTransfer,
-  UserNotificationOwnership,
-  UserNotificationTransfer,
-  UserNotificationTrust,
-} from '../hooks/subgraph/useNotifications'
+import { UserNotification } from '../hooks/subgraph/useNotifications'
+import { circlesToTC } from './circleConversor'
+import formatNumber from './formatNumber'
 import { getItem, hasItem, isAvailable, removeItem, setItem } from './storage'
 import { NotificationType } from '@/types/subgraph/__generated__/globalTypes'
 
@@ -19,11 +14,11 @@ export const getLastSeen = () => {
   if (isAvailable() && hasLastSeen()) {
     const value = getItem(LAST_SEEN_NAME)
     if (value) {
-      return value
+      return parseInt(value)
     }
   }
 
-  return Date.now().toString()
+  return null
 }
 
 export const setLastSeen = (lastSeen: string) => {
@@ -34,83 +29,64 @@ export const removeLastSeen = () => {
   removeItem(LAST_SEEN_NAME)
 }
 
-export const updateLastSeen = () => {
-  const currentDateSeconds = new Date().getTime()
-  const timestamp = Math.round(currentDateSeconds / 1000)
-  const newTs = String(timestamp)
-  setLastSeen(String(timestamp))
-  return newTs
+export const updateLastSeen = (ts: number) => {
+  setLastSeen(String(ts))
+}
+
+export const getUnseenNotifications = (notifications: UserNotification[]) => {
+  const lastSeen = getLastSeen()
+  if (lastSeen) {
+    return notifications.filter((notification) => notification.time > lastSeen)
+  }
+  return notifications
 }
 
 export type ActivityMessage = {
   addressActor: string
   addressOrigin: string
   addressTarget: string
-  data: UserNotification
-  messageId: string
+  message: string
+  notification: UserNotification
   date: string
 }
 
-export const formatActivityMessage = (
-  data: UserNotification,
-  safeAddress: string,
-): ActivityMessage => {
-  let addressActor
-  let addressOrigin
-  let addressTarget
-  let messageId
-  let _data
+// @TODO notification is not receiving the group name in all cases
+export const formatActivityMessage = (notification: UserNotification): ActivityMessage => {
+  const addressActor = ''
+  const addressOrigin = ''
+  const addressTarget = ''
+  let message = ''
 
-  if (data.type === NotificationType.TRANSFER) {
-    _data = data as UserNotificationTransfer
-    addressOrigin = _data.transfer.from
-    addressTarget = _data.transfer.to
-    if (_data.transfer.from === ZERO_ADDRESS) {
-      // I've received Circles from the Hub (UBI)
-      messageId = 'ReceivedUBI'
-    } else {
-      messageId = 'SentUBI'
-    }
-  } else if (data.type === NotificationType.HUB_TRANSFER) {
-    _data = data as UserNotificationHubTransfer
-    addressOrigin = _data.hubTransfer.from
-    addressTarget = _data.hubTransfer.to
-    if (_data.hubTransfer.to === safeAddress) {
-      // I've received Circles from someone
-      messageId = 'ReceivedCircles'
-      addressActor = _data.hubTransfer.from
-    } else {
-      // I've sent Circles to someone
-      messageId = 'SentCircles'
-      addressActor = _data.hubTransfer.to
-    }
-  } else if (data.type === NotificationType.OWNERSHIP) {
-    _data = data as UserNotificationOwnership
-    addressOrigin = safeAddress
-    if (_data.ownership.adds) {
-      addressTarget = _data.ownership.adds
-      messageId = 'AddedToSafe'
-    } else {
-      addressTarget = _data.ownership.removes
-      messageId = 'RemovedFromSafe'
-    }
-  } else if (data.type === NotificationType.TRUST) {
-    _data = data as UserNotificationTrust
-    messageId = 'TrustCircleUser'
-    addressOrigin = safeAddress
-    addressTarget = _data.trust.user
+  if (notification.type === NotificationType.GROUP_CREATION && notification.groupCreation) {
+    const groupName = notification.groupCreation.name
+    message = `You have created the ${groupName} group`
   }
-
-  if (!messageId) {
-    messageId = `Unknown activity type: ${data.type}`
+  if (notification.type === NotificationType.GROUP_ADD_MEMBER && notification.groupAddMember) {
+    const groupName = notification.groupAddMember.group
+    message = `You are now member of ${groupName} group`
+  }
+  if (
+    notification.type === NotificationType.GROUP_REMOVE_MEMBER &&
+    notification.groupRemoveMember
+  ) {
+    const groupName = notification.groupRemoveMember.group
+    message = `You are not member of ${groupName} group anymore`
+  }
+  if (notification.type === NotificationType.GROUP_MINT && notification.groupMint) {
+    const groupName = notification.groupMint.group
+    const tc = formatNumber(circlesToTC(notification.groupMint.amount))
+    message = `You have minted ${tc} CRC on ${groupName} group`
+  }
+  if (!message) {
+    message = `Unknown activity type: ${notification.type}`
   }
 
   return {
     addressActor: addressActor ?? '',
     addressOrigin: addressOrigin ?? '',
     addressTarget: addressTarget ?? '',
-    data,
-    messageId,
-    date: new Date(data.time).toString(), // @TODO remember to parse date correctly
+    notification,
+    message,
+    date: new Date(notification.time).toString(), // @TODO remember to format date correctly
   }
 }
