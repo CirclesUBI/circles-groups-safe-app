4af4d4542023813e2419c07d19c59009f99768eb
Shisus <jesus.laime@camba.coop>
2022-08-05 14:17:28 -0300

    fixes: use groups to filter tokens owned by a user safe address

--- src/hooks/useCirclesBalance.ts
+++ src/hooks/useCirclesBalance.ts
@@ -6,6 +6,7 @@ import SafeAppsSDK from '@gnosis.pm/safe-apps-sdk/dist/src/sdk'
 
 import { circlesToTC } from '../utils/circleConversor'
 import formatNumber from '../utils/formatNumber'
+import { useGroupMints } from './subgraph/useGroupMints'
 import { useSafeBalances } from './useSafeBalances'
 
 const CRC = 'CRC'
@@ -16,8 +17,7 @@ const CRC = 'CRC'
  * @TODO we might want to fetch their balances using the SG
  */
 export const getCirclesFromBalances = (tokenBalances: TokenBalance[]) => {
-  const crcTokens = tokenBalances.filter((tokenBalance) => tokenBalance.tokenInfo.symbol === CRC)
-  const circlesAmounts = crcTokens.map((crcToken) => crcToken.balance)
+  const circlesAmounts = tokenBalances.map((crcToken) => crcToken.balance)
   let accumulatorBN = BigNumber.from(0)
   circlesAmounts.forEach((circlesAmount) => {
     const circlesAmountBN = BigNumber.from(circlesAmount)
@@ -26,13 +26,32 @@ export const getCirclesFromBalances = (tokenBalances: TokenBalance[]) => {
   return accumulatorBN.toString()
 }
 
-export const useCirclesBalance = (sdk: SafeAppsSDK) => {
+/**
+ * @description we fetch the group tokens owned by a user in the following way:
+ * - we fetch the ERC20 user tokens using the gnosis useSafeBalances hook
+ * - if a token has the symbol CRC then it's a normal CRC token
+ * - if a token address belong to the minted tokens then it's a group CRC token
+ *
+ * @todo a better way should be using the Balance schema from the SG but ATM
+ * it does not create balances for group tokens
+ *
+ */
+export const useCirclesBalance = (safeAddress: string, sdk: SafeAppsSDK) => {
   const [tokenBalances] = useSafeBalances(sdk)
+  const { groupMints } = useGroupMints(safeAddress)
+
   const circles = useMemo(() => {
-    const numericBalance = getCirclesFromBalances(tokenBalances)
+    const groupAddresses = groupMints.map((groupMint) => groupMint.group.toLowerCase())
+    const tokens = tokenBalances.filter((tokenBalance) => {
+      const isCRC = tokenBalance.tokenInfo.symbol === CRC
+      const tokenBalanceAddress = tokenBalance.tokenInfo.address.toLowerCase()
+      const isGroup = groupAddresses.includes(tokenBalanceAddress)
+      return isCRC || isGroup
+    })
+    const numericBalance = getCirclesFromBalances(tokens)
     const tcBalance = circlesToTC(numericBalance)
     return formatNumber(tcBalance)
-  }, [tokenBalances])
+  }, [tokenBalances, groupMints])
 
   return { circles }
 }
