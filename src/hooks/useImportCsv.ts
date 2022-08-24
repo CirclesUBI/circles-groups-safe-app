5db1ecd56d804fac295cc71a3e3d1da331c432c8
Marcelo Rubini <rubini.marcelo@gmail.com>
2022-08-24 16:27:10 -0300

    adds user already group member validation

--- src/hooks/useImportCsv.ts
+++ src/hooks/useImportCsv.ts
@@ -4,6 +4,7 @@ import { isAddress } from '@ethersproject/address'
 import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk'
 
 import { getUsers } from '../utils/circlesGardenAPI'
+import { useGroupMembersByGroupId } from './subgraph/useGroupMembers'
 import useSafeTransaction from '@/src/hooks/useSafeTransaction'
 import { useWeb3Connected } from '@/src/providers/web3ConnectionProvider'
 import encodeGCTTransaction from '@/src/utils/contracts/encodeGCTTransaction'
@@ -14,14 +15,24 @@ export const useImportCsv = (groupAddress: string) => {
   const { sdk } = useSafeAppsSDK()
   const { isAppConnected, web3Provider } = useWeb3Connected()
   const { execute } = useSafeTransaction(sdk)
+  const { groupMembers } = useGroupMembersByGroupId(groupAddress)
 
   const addUsersToGroup = useCallback(
     async (userAddresses: string[]) => {
       try {
+        if (!userAddresses.length) {
+          throw new Error('No user addresses where given')
+        }
+        // TODO: validate existent users all together with [userAddresses]
+        // wrap isCirclesUser with a Promise.all()
         const isCirclesUser = async (userAddress: string) => {
           const fetchedUsers = await getUsers([userAddress])
           return fetchedUsers.length !== 0
         }
+        const isAlreadyAMember = (userAddress: string) =>
+          groupMembers.some(
+            ({ safeAddress }) => safeAddress.toLowerCase() === userAddress.toLowerCase(),
+          )
         const getUserToken = async (userAddress: string) => {
           if (!isAppConnected) {
             throw new Error('App is not connected')
@@ -38,7 +49,11 @@ export const useImportCsv = (groupAddress: string) => {
         }
         const provider = web3Provider.getSigner()
         const userTokenPromises = userAddresses.map(async (userAddress) => {
-          if (isAddress(userAddress) && (await isCirclesUser(userAddress))) {
+          if (
+            isAddress(userAddress) &&
+            (await isCirclesUser(userAddress)) &&
+            !isAlreadyAMember(userAddress)
+          ) {
             const userToken = await getUserToken(userAddress)
             return userToken
           }
@@ -50,6 +65,9 @@ export const useImportCsv = (groupAddress: string) => {
           throw new Error('No user tokens for given addresses')
         }
         const filteredUserTokens = userTokens.filter(Boolean) // removes cases where userToken is empty string
+        if (!filteredUserTokens.length) {
+          throw new Error('No filtered user tokens for given addresses')
+        }
 
         const encodedTxPromises = filteredUserTokens.map(async (userToken) => {
           const encodedTx = await encodeGCTTransaction(groupAddress, provider, 'addMemberToken', [
@@ -71,7 +89,7 @@ export const useImportCsv = (groupAddress: string) => {
         setLoading(false)
       }
     },
-    [isAppConnected, web3Provider, execute, groupAddress],
+    [isAppConnected, web3Provider, execute, groupMembers, groupAddress],
   )
   return {
     addUsersToGroup,
