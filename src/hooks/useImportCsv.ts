ffe76798755696517a8feb64f3cb5d7210267079
Shisus <jesus.laime@camba.coop>
2022-08-24 17:35:08 -0300

    refactor: reduces amount of api calls when validating that user exists

--- src/hooks/useImportCsv.ts
+++ src/hooks/useImportCsv.ts
@@ -17,56 +17,54 @@ export const useImportCsv = (groupAddress: string) => {
   const { execute } = useSafeTransaction(sdk)
   const { groupMembers } = useGroupMembersByGroupId(groupAddress)
 
+  const isAlreadyAMember = useCallback(
+    (userAddress: string) =>
+      groupMembers.some(
+        ({ safeAddress }) => safeAddress.toLowerCase() === userAddress.toLowerCase(),
+      ),
+    [groupMembers],
+  )
+
   const addUsersToGroup = useCallback(
     async (userAddresses: string[]) => {
       try {
-        if (!userAddresses.length) {
-          throw new Error('No user addresses where given')
-        }
-        // TODO: validate existent users all together with [userAddresses]
-        // wrap isCirclesUser with a Promise.all()
-        const isCirclesUser = async (userAddress: string) => {
-          const fetchedUsers = await getUsers([userAddress])
-          return fetchedUsers.length !== 0
-        }
-        const isAlreadyAMember = (userAddress: string) =>
-          groupMembers.some(
-            ({ safeAddress }) => safeAddress.toLowerCase() === userAddress.toLowerCase(),
-          )
-        const getUserToken = async (userAddress: string) => {
-          if (!isAppConnected) {
-            throw new Error('App is not connected')
-          }
-          const userToken = await hubCall(web3Provider, 'userToToken', [userAddress])
-          if (!userToken) {
-            throw new Error('User Token does not exists')
-          }
-          return userToken
-        }
         setLoading(true)
         if (!isAppConnected) {
           throw new Error('App is not connected')
         }
         const provider = web3Provider.getSigner()
-        const userTokenPromises = userAddresses.map(async (userAddress) => {
-          if (
-            isAddress(userAddress) &&
-            (await isCirclesUser(userAddress)) &&
-            !isAlreadyAMember(userAddress)
-          ) {
-            const userToken = await getUserToken(userAddress)
-            return userToken
-          }
-          return ''
+
+        if (!userAddresses.length) {
+          throw new Error('No user addresses where given')
+        }
+        const addresses = userAddresses.filter((address) => isAddress(address))
+        if (!addresses.length) {
+          throw new Error('No valid user addresses where given')
+        }
+        const circlesUsers = await getUsers(addresses)
+
+        const existingUserAddresses = addresses.filter((address) =>
+          circlesUsers.some((user) => user.safeAddress.toLowerCase() === address.toLowerCase()),
+        )
+        if (!existingUserAddresses.length) {
+          throw new Error('User addresses given does not exist in the system')
+        }
+
+        const nonAlreadyMemberAddresses = existingUserAddresses.filter(
+          (address) => !isAlreadyAMember(address),
+        )
+        if (!nonAlreadyMemberAddresses.length) {
+          throw new Error('User addresses given are already members of the group')
+        }
+
+        const userTokenPromises = userAddresses.map(async (userAddress: string) => {
+          const userToken = await hubCall(web3Provider, 'userToToken', [userAddress])
+          return userToken ?? ''
         })
         const userTokens = await Promise.all(userTokenPromises)
-
+        const filteredUserTokens = userTokens.filter(Boolean) // removes cases where userToken is empty string
         if (!userTokens.length) {
           throw new Error('No user tokens for given addresses')
-        }
-        const filteredUserTokens = userTokens.filter(Boolean) // removes cases where userToken is empty string
-        if (!filteredUserTokens.length) {
-          throw new Error('No filtered user tokens for given addresses')
         }
 
         const encodedTxPromises = filteredUserTokens.map(async (userToken) => {
@@ -75,21 +73,19 @@ export const useImportCsv = (groupAddress: string) => {
           ])
           return encodedTx
         })
-
         const encondedTxs = await Promise.all(encodedTxPromises)
-
         if (!encondedTxs.length) {
           throw new Error('No AddMemberToken txs to execute')
         }
+
         await execute(encondedTxs)
-        // TODO: handle GroupMembers state update
       } catch (e) {
         console.log(e)
       } finally {
         setLoading(false)
       }
     },
-    [isAppConnected, web3Provider, execute, groupMembers, groupAddress],
+    [isAppConnected, web3Provider, execute, groupAddress, isAlreadyAMember],
   )
   return {
     addUsersToGroup,
