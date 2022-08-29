a2c9f53df424ab06e2f5858d50fdad704b7ecd9f
Shisus <jesus.laime@camba.coop>
2022-08-29 13:13:51 -0300

    fixes error when showing notification success modal with an invalid url link

--- src/hooks/useSafeTransaction.tsx
+++ src/hooks/useSafeTransaction.tsx
@@ -2,35 +2,31 @@ import { useCallback, useMemo } from 'react'
 
 import { BaseTransaction, SendTransactionsResponse } from '@gnosis.pm/safe-apps-sdk'
 import SafeAppsSDK from '@gnosis.pm/safe-apps-sdk/dist/src/sdk'
+import {
+  TransactionDetails,
+  TransactionStatus,
+} from '@gnosis.pm/safe-apps-sdk/node_modules/@gnosis.pm/safe-react-gateway-sdk'
 import { toast } from 'react-hot-toast'
 
+import { retry } from '../utils/tools'
 import { getGnosisExplorerUrl } from '../web3/explorerUrls'
 import { notify } from '@/src/components/toast/Toast'
 import { FAILED_TYPE, SUCCESS_TYPE, WAITING_TYPE } from '@/src/components/toast/types'
 import { createTransactionError } from '@/src/utils/TransactionError'
 
 export default function useSafeTransaction(sdk: SafeAppsSDK) {
-  /**
-   * @todo this is a really BAD approach to handle confirmations:
-   * a better way would be using a library which allow to wait for X time before
-   * recalling a function
-   */
   const waitForConfirmations = useCallback(
     async (safeTxHash: string) => {
-      let attempts = 0
-      let finished = false
-      let txHash = ''
-      if (!sdk) return txHash
+      if (!sdk) return ''
 
-      while (!finished || attempts < 15) {
-        const txDetails = await sdk.txs.getBySafeTxHash(safeTxHash)
-        finished = txDetails && txDetails.txStatus !== 'AWAITING_EXECUTION'
-        if (txDetails.txHash) {
-          txHash = txDetails.txHash
-        }
-        attempts += 1
+      const getDetails = (_safeTxHash: string) => {
+        return sdk.txs.getBySafeTxHash(_safeTxHash)
       }
-      return txHash
+      const untilFinished = (txDetails: TransactionDetails) => {
+        return txDetails.txStatus !== TransactionStatus.AWAITING_CONFIRMATIONS
+      }
+      const details = await retry(safeTxHash, getDetails, untilFinished, 0)
+      return details?.txHash ?? ''
     },
     [sdk],
   )
@@ -54,14 +50,11 @@ export default function useSafeTransaction(sdk: SafeAppsSDK) {
         notify({ type: WAITING_TYPE, message: 'waiting for approval' })
         txHash = await waitForConfirmations(safeTxHash)
         txExplorerUrl = getGnosisExplorerUrl(txHash)
-
         notify({ type: SUCCESS_TYPE, explorerUrl: txExplorerUrl })
         if (onSuccess) onSuccess()
         return txResponse.safeTxHash
       } catch (e: any) {
         toast.dismiss()
-        console.log({ e })
-
         const error = createTransactionError(e)
         if (error.code === 4001) {
           notify({ type: FAILED_TYPE, message: 'User denied signature' })
@@ -69,7 +62,6 @@ export default function useSafeTransaction(sdk: SafeAppsSDK) {
           return null
         }
         console.error('Transaction error', error.message)
-        // notify({ type: FAILED_TYPE, explorerUrl: txExplorerUrl })
         notify({ type: FAILED_TYPE, message: error.message })
         if (onError) onError()
         return null
