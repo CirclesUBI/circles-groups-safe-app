0666b3fccac577def4b607824e176e8aabcb6639
jaensen <4954577+jaensen@users.noreply.github.com>
2023-04-28 04:56:15 +0200

    "next": "12.3.0", images.unoptimized: true

--- next.config.js
+++ next.config.js
@@ -2,28 +2,8 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['circles-ubi.s3.amazonaws.com', 'dolphin-app-27r5c.ondigitalocean.app']
+    unoptimized: true,
+    domains: ['app.safe.global']
   },
-  async headers() {
-    return [
-      {
-        // Allow requests from example.com
-        source: '/',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://app.safe.global',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
+  output: 'export'
 }
